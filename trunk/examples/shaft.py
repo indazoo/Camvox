d0a1a0fd3803d9f2a43d35286f96bb1da2f4e7a6
takev <takev@71cc4548-2e10-4f5d-815e-4792b18e0a7e>
2008-08-11 22:29:11 +0000

    shaft fits on my blank, website has download page

--- trunk/examples/shaft.py
+++ trunk/examples/shaft.py
@@ -5,61 +5,55 @@ import sys
 from math import *
 
 def linkage():	
-	b = Box(5.0, 4.0, 2.0)
-	b.translate(camvox.Vector(2.5, 0.0, 0.0))
+	b = Box(3.0, 4.0, 2.0)
+	b.translate(camvox.Vector(1.5, 0.0, 0.0))
 
 	c = CappedCylinder(2.0, 2.0)
-	c.translate(camvox.Vector(5.0, 0.0, 0.0))
+	c.translate(camvox.Vector(3.0, 0.0, 0.0))
 
 	return Union(b, c)
 
-c10 = CappedCylinder(2.0, 10.0)
-c10.translate(camvox.Vector(0.0, 0.0, 15.0))
+def crank_shaft():
+	c10 = CappedCylinder(2.0, 10.0)
+	c10.translate(camvox.Vector(0.0, 0.0, 15.0))
 
-l12 = linkage()
-l12.translate(camvox.Vector(0.0, 0.0, 11.0))
+	l12 = linkage()
+	l12.translate(camvox.Vector(0.0, 0.0, 11.0))
 
-c15 = CappedCylinder(1.5, 9.0)
-c15.translate(camvox.Vector(5.0, 0.0, 7.5))
+	c15 = CappedCylinder(1.5, 9.0)
+	c15.translate(camvox.Vector(3.0, 0.0, 7.5))
 
-l17 = linkage()
-l17.translate(camvox.Vector(0.0, 0.0, 4.0))
+	l17 = linkage()
+	l17.translate(camvox.Vector(0.0, 0.0, 4.0))
 
-c20 = CappedCylinder(2.0, 10.0)
+	c20 = CappedCylinder(2.0, 10.0)
 
-l22 = linkage()
-l22.rotate(camvox.Vector(0.0, 0.0, 1.0), pi)
-l22.translate(camvox.Vector(0.0, 0.0, -4.0))
+	l22 = linkage()
+	l22.rotate(camvox.Vector(0.0, 0.0, 1.0), pi)
+	l22.translate(camvox.Vector(0.0, 0.0, -4.0))
 
-c25 = CappedCylinder(1.5, 9.0)
-c25.translate(camvox.Vector(-5.0, 0.0, -7.5))
+	c25 = CappedCylinder(1.5, 9.0)
+	c25.translate(camvox.Vector(-3.0, 0.0, -7.5))
 
-l27 = linkage()
-l27.rotate(camvox.Vector(0.0, 0.0, 1.0), pi)
-l27.translate(camvox.Vector(0.0, 0.0, -11.0))
+	l27 = linkage()
+	l27.rotate(camvox.Vector(0.0, 0.0, 1.0), pi)
+	l27.translate(camvox.Vector(0.0, 0.0, -11.0))
 
-c30 = CappedCylinder(2.0, 10.0)
-c30.translate(camvox.Vector(0.0, 0.0, -15.0))
+	c30 = CappedCylinder(2.0, 10.0)
+	c30.translate(camvox.Vector(0.0, 0.0, -15.0))
 
+	return Union(c10, l12, c15, l17, c20, l22, c25, l27, c30)
 
 # Move to center
-u = camvox.CSGUnion()
-u.add(c10)
-u.add(l12)
-u.add(c15)
-u.add(l17)
-u.add(c20)
-u.add(l22)
-u.add(c25)
-u.add(l27)
-u.add(c30)
-u.translate(camvox.Vector(50.0, 50.0, 50.0))
+
+cr = crank_shaft()
+cr.rotate(camvox.Vector(0.0, 1.0, 0.0), -0.5 * pi)
+cr.translate(camvox.Vector(25.0, 10.0, 10.0))
 
 
-tree = camvox.VoxTree(100.0)
+tree = camvox.VoxTree(50.0)
 tree.max_depth = 8
-
-tree.addCSGObjectOR(u, 1)
+tree.addCSGObjectOR(cr, 1)
 
 print >>sys.stderr, tree.nr_nodes_created, tree.nr_nodes_destroyed, tree.nr_nodes_created - tree.nr_nodes_destroyed
 tree.generatePOVCode()
