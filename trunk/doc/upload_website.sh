99910e8b1c4e3c2ab4b98d10605fc00842da05e8
takev <takev@71cc4548-2e10-4f5d-815e-4792b18e0a7e>
2008-08-15 09:25:16 +0000

    doxygen output on the website

--- trunk/doc/upload_website.sh
+++ trunk/doc/upload_website.sh
@@ -1,3 +1,6 @@
 
-cd website; scp -r . takev@shell.sourceforge.net:/home/groups/c/ca/camvox/htdocs
+#cd website; scp -r . takev@shell.sourceforge.net:/home/groups/c/ca/camvox/htdocs
+./generate_website
+
+rsync -rvpz --copy-unsafe-links --delete --delete-excluded --exclude='*.graffle' --exclude=.svn --rsh="ssh -l takev" website/ shell.sourceforge.net:/home/groups/c/ca/camvox/htdocs/
 
